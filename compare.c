5eacdbe059710b51a377efe645833db790e618f1
Amandio-365 <18502293796@163.com>
2026-01-26 11:31:36 +0800

    first commit

---
+++ compare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int n;
+    printf("Please input the number : ");
+    scanf_s("%d", &n);
+    if (n% 2 == 0)
+    {
+        printf("n是一个偶数");
+    }
+    else
+    {
+        printf("n是个记述");
+    }
+}
